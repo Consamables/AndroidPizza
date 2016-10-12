369a55dda28ed976acc1df1e1c434e6537713e12
wolfd <2dawolf@gmail.com>
2016-10-12 15:38:58 -0400

    Add material buttons to bottom of wizard view.

---
+++ app/src/main/java/pizza/olin/consamables/WizardActivity.java
@@ -0,0 +1,13 @@
+package pizza.olin.consamables;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WizardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wizard);
+    }
+}
